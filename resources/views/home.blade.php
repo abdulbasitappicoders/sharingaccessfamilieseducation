940e4bccac84c0736f0e091bf1ebef9862ae0d89
abbasitmemon <abbasitmemon1@yahoo.com>
2023-01-11 21:24:04 +0500

    Changes in dashboard 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -114,6 +114,7 @@
     <div>
         <canvas id="myChart" height="100" ></canvas>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
     <script type="text/javascript">
     
         var ctx = document.getElementById("myChart").getContext('2d');
