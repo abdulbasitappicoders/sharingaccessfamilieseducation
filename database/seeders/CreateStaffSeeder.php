e9d862043b017e7ace879a370f922651a1e67473
Irfan <hirfan875@gmail.com>
2023-02-06 19:16:58 +0500

    staff seeder

---
+++ database/seeders/CreateStaffSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class CreateStaffSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (!User::where('role', 'staff')->exists()) {
+            User::create([
+                'username' => 'staff',
+                'first_name' => 'staff',
+                'email' => 'staff@safe.com',
+                'gender' => 'male',
+                'vehicle_type' => 'car',
+                'status' => '1',
+                'role' => 'staff',
+                'password' => Hash::make("123456789"),
+            ]);
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(InitialSettingsSeeder::class);
         $this->call(CreateAdminSeeder::class);
+        $this->call(CreateStaffSeeder::class);
     }
 }
