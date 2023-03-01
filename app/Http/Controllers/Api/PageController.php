fe49913a8ed57199c9cc44537464c9bb83aa17d4
Irfan <hirfan875@gmail.com>
2023-03-01 13:17:04 +0500

    fixes

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -13,14 +13,15 @@ class PageController extends Controller
     {
         try {
             $page = Page::first();
-            /*$page->termsCondition = strip_tags($page->termsCondition);
+            $page->termsCondition = strip_tags($page->termsCondition);
             $page->privacyPolicy = strip_tags($page->privacyPolicy);
             $page->help = strip_tags($page->help);
-            $page->communityGroup = strip_tags($page->communityGroup);*/
-            $page->termsCondition = html_entity_decode($page->termsCondition);
-            $page->privacyPolicy = html_entity_decode($page->privacyPolicy);
-            $page->help = html_entity_decode($page->help);
-            $page->communityGroup = html_entity_decode($page->communityGroup);
+            $page->communityGroup = strip_tags($page->communityGroup);
+
+            /*$page->termsCondition = htmlspecialchars_decode($page->termsCondition);
+            $page->privacyPolicy = htmlspecialchars_decode($page->privacyPolicy);
+            $page->help = htmlspecialchars_decode($page->help);
+            $page->communityGroup = htmlspecialchars_decode($page->communityGroup);*/
             return apiresponse(true, 'data found',$page);
         } catch (Exception $e) {
             return apiresponse(false, $e->getMessage());
