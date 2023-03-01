5f1c474b175d00ba4983bc5f78a4a0f4974033be
Irfan <hirfan875@gmail.com>
2023-03-01 12:07:33 +0500

    fixes

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -13,10 +13,14 @@ class PageController extends Controller
     {
         try {
             $page = Page::first();
-            $page->termsCondition = strip_tags($page->termsCondition);
+            /*$page->termsCondition = strip_tags($page->termsCondition);
             $page->privacyPolicy = strip_tags($page->privacyPolicy);
             $page->help = strip_tags($page->help);
-            $page->communityGroup = strip_tags($page->communityGroup);
+            $page->communityGroup = strip_tags($page->communityGroup);*/
+            $page->termsCondition = $page->termsCondition;
+            $page->privacyPolicy = $page->privacyPolicy;
+            $page->help = $page->help;
+            $page->communityGroup = $page->communityGroup;
             return apiresponse(true, 'data found',$page);
         } catch (Exception $e) {
             return apiresponse(false, $e->getMessage());
