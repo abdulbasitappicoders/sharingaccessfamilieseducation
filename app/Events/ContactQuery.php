836bb5f86abd3a8fd91666865f82d577102d9d66
mparyal371 <mparyal371@gmail.com>
2023-08-01 14:34:52 +0500

    safe--

---
+++ app/Events/ContactQuery.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactQuery implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $data;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'data' => $this->data,
+        ];
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('contact-query');
+    }
+
+    public function broadcastAs()
+    {
+        return 'contactQuery';
+    }
+
+}
